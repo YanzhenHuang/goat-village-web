f8a925119757db1e6191476de39bf56487d27613
phoiex <149285395+phoiex@users.noreply.github.com>
2024-09-07 17:45:14 +0800

    Push journal

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -2,16 +2,44 @@ import Link from "next/link";
 import { useState } from "react";
 import NavBar from "@/components/NavBar";
 
+import { StaggeredSection } from "@/components/StaggeredSection";
+import { TitledTextBlock } from "@/components/TitledTextBlock";
+import ImageGallery from "@/components/ImageGallery";
+import { shuffle } from "@/utils/func_tools";
+
+
+import {texts} from "@/public/blogs/gpz"
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <title>Goat Village - 博客</title>
       {/* Header */}
       <NavBar />
-      <section className={`flex flex-col items-center justify-center w-full`}>
-        <img src={`../images/hyz_logo/logo_flower.png`} />
-        <div>s</div>
-      </section>
+      <StaggeredSection direction={`right`}>
+        {/* 标题-文字 */}
+        <TitledTextBlock
+          content={{
+            title: "羊村出行",
+            annotation: "[IP属地] 中国香港",
+            text: texts[0]
+          }}
+          direction={`right`} />
+
+        <ImageGallery
+          galleryRootDir={`../images/photos/index/travel/`}
+          imageFileNames={
+            shuffle([
+              `depart.jpg`,
+              `dock.jpg`,
+              `gpz_sleep.jpg`,
+              `hk_road.jpg`,
+              `hk_street.jpg`,
+              `lrx_icecream_car.jpg`,
+              `lrx_sleep.jpg`])
+          }
+          bigImgPos={{ x: "right" }} />
+      </StaggeredSection>
     </main>
   );
 }
